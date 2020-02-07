e0fdf091f2d934b1419cb09cd963e79400812b50
Brett Regnier <bretternestregnier@gmail.com>
2020-02-06 21:35:05 -0700

    finished assignment 2 question 4

--- asn2/classifier.py
+++ asn2/classifier.py
@@ -1,8 +1,11 @@
-import nltk
-import random
-from nltk.corpus import brown
-from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import brown
+import random
+import nltk
+nltk.download("brown")  # just incase
+
 
 # TODO
 # Take each word from each category and classify them as their category.
@@ -12,40 +15,73 @@ from nltk.corpus import stopwords
 # OR Tokenize each word in a text, from the brown corpus, and take its category and create training data from that
 # similar to how the examples in class are. From there we can feed in texts that need to be classified. Should work better.
 
-# create features based on the sentence and the category it is associated with
-stopwords = stopwords.words('english')
-lemmatizer = WordNetLemmatizer()
-features = []
-for c in brown.categories():
-    sentences = list(brown.sents(categories=c))
-    sentence = ""
-    for s in sentences:
-        # turn the tokenized sentence into a alpha numeric without punctuation
-        # and remove stopwords
-        for word in s:
-            word = lemmatizer.lemmatize(word)
-            if (word not in stopwords) and (word.isalnum()):
-                sentence += word.lower() + " "
-                feature = ({"word": word}, c)
+def main():
+    features = None
+    shuffles = 0
+    
+    choice = ""
+    while choice != "exit":
+        choice = ""
+        while choice not in ["run", "exit"]:
+            choice = input("Would you like to classify the Brown corpus? [Run, Exit]\n>").lower()
+        
+        if choice == "exit":
+            break
+
+        shuffles = 0
+        while shuffles < 1 or shuffles > 100:
+            shuffles = int(input("How many times would you like to shuffle and run? [1-100]\n>"))
+            print("")
+    
+        # Create features if not already made
+        if features is None:
+            print("Creating features...")
+            features = CreateFeatures()
+
+        # shuffle, train and test up to x amount
+        for i in range(shuffles):
+            print("Iteration:", i)
+            random.shuffle(features)
+
+            # split the data into training and data
+            split = int(len(features) * .7)
+            trainingData = features[:split]
+            testingData = features[split:]
+
+            # train the classifier
+            print("  Training Classifier...")
+            classifier = nltk.NaiveBayesClassifier.train(trainingData)
+            print("  DING! - Done training!")
+            print("  Training set:", len(trainingData), "items")
+            print("  Testing set:", len(testingData), "items")
+            print("  Classifying testing data...")
+            accuracy = nltk.classify.accuracy(classifier, testingData) * 100 # turn into percent.
+            print("  Accuracy:", "{0:.4f}".format(round(accuracy,4)) + "%")
+            print("")
+
+
+def CreateFeatures():
+    features = []
+    sw = stopwords.words('english')
+    lemmatizer = WordNetLemmatizer()
+    stemmer = PorterStemmer()  # yielded an extra 0.1% so I kept it in.
+    
+    for c in brown.categories():
+        for d in brown.fileids(categories=c):
+            words = brown.words(fileids=d)
+            extracted_words = ""
+            for w in words:
+                w = lemmatizer.lemmatize(w)
+                w = stemmer.stem(w)
+                if (w not in sw) and (w.isalnum()):
+                    extracted_words += w.lower() + " "
+                    # feature_words.append({"w": w, "c": c})
+                    # tokens.append(w)
+                feature = ({"words": extracted_words}, c)
                 features.append(feature)
-        # sentence = ' '.join([e for e in s if e.isalpha()])
-        # feature = ({"word": word}, c)
-        # features.append(feature)
 
-# shuffle the data so the classifier will recieve a different set of
-# training and testing each time.
-random.shuffle(features)
+    return features
 
-# split into 70% and 30%
-trainingSize = int(len(features) * .7)
-trainingData = features[:trainingSize]
-testingData = features[trainingSize:]
 
-# train the classifier
-classifier = nltk.NaiveBayesClassifier.train(trainingData)
-print("Done training")
-
-print(classifier.classify(
-    {'sentence': 'dan morgan told would forget ann turner'}))
-
-print("Accuracy on testing data:", nltk.classify.accuracy(classifier, testingData))
+if __name__ == "__main__":
+    main()
