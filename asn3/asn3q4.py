5ae919736f4351b1d5fb0975cc33418cb8ba2138
brettregnier <bretternestregnier@gmail.com>
2020-03-22 23:58:28 -0600

    finished

--- asn3/asn3q4.py
+++ asn3/asn3q4.py
@@ -9,9 +9,6 @@ from gensim.models import TfidfModel
 from gensim.models import Word2Vec, WordEmbeddingSimilarityIndex
 from gensim.similarities import SoftCosineSimilarity, SparseTermSimilarityMatrix, MatrixSimilarity
 
-import os.path
-
-
 def main():
     tfidf = None
     word2vec = None
@@ -51,8 +48,8 @@ def main():
                 texts.append(words[:len(words)//2])
                 texts.append(words[len(words)//2:])
                 # build a dictionary for me to use later
-                browndict[c+"1"] = words[:len(words)//2]
-                browndict[c+"2"] = words[len(words)//2:]
+                browndict[c+"1/2"] = words[:len(words)//2]
+                browndict[c+"2/2"] = words[len(words)//2:]
 
 
         # create the corpora dictionary built from gensim
@@ -85,53 +82,29 @@ def main():
         if choice == "tfidf":
             # Print out the code
             keys = list(browndict.keys())
-            for i in range(len(keys)):
+            for i in range(len(keys)-1):
+                # Convert to a bag of words and to a tfidf vector, then query it.
                 query_bow = corporadict.doc2bow(browndict[keys[i]])
                 query_tfidf = tfidf[query_bow]
 
                 # Get the similarity of every cluster
                 query_similarity = similarityMatrix[query_tfidf]
-                mx = 0
-                j = 0
-                for j in range(len(query_similarity)):
-                    if i == j:
-                        continue
-                    
+                for j in range(i+1,len(query_similarity)):
                     sim = query_similarity[j]
-
-                    # find the maximum that is not itself
-                    if sim > mx:
-                        mx = sim
-                        s = keys[i] + " and " + keys[j]
                     print(keys[i], "and", keys[j], "have a similarity of:",sim)
-                maxes[s] = mx
-                i += 1
+                print("")
         elif choice == "word2vec":
-            # Print out the code
             keys = list(browndict.keys())
-            for i in range(len(keys)):
+            for i in range(len(keys)-1):
+                # Convert to a bag of words and query it
                 query_bow = corporadict.doc2bow(browndict[keys[i]])
 
                 # Get the similarity of every cluster
                 query_similarity = WV_SimilarityMatrix[query_bow]
-                mx = 0
-                j = 0
-                for j in range(len(query_similarity)):
-                    if i == j:
-                        continue
-                    
+                for j in range(i+1,len(query_similarity)):
                     sim = query_similarity[j]
-                    
-                    # find the maximum that is not itself
-                    if sim > mx:
-                        mx = sim
-                        s = keys[i] + " and " + keys[j]
-                    print(keys[i], "is", keys[j], "have a similarity of:",sim)
-                maxes[s] = mx
-                i += 1
-        
-        for key in maxes:
-            print("Max similarity for", key, "=", maxes[key])
+                    print(keys[i], "and", keys[j], "have a similarity of:",sim)
+                print("")
 
 def NormalizeWords(words):
     tokens = []
