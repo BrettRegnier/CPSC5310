964ed50854fd5597d5f888a07d299dc89a4b6e7f
brettregnier <bretternestregnier@gmail.com>
2020-04-09 17:20:11 -0600

    finished with the assignment

--- asn4/asn4q3.py
+++ asn4/asn4q3.py
@@ -29,41 +29,56 @@ import os
 ########################################################################################################
 
 def main():
-    word2vec = None
-    perceptron = None
-    model = None
-
     # prepare the data
     print("Normalizing words from each document given their categories...")
+
+    docw2v = None
+    boww2v = None
+
     docs_vec = []
-    docs_cat = []
+    docs_cat_mat = []
+
+    bow_vec = []
+    bow_cat_mat = []
     for c in brown.categories():
         for _id in brown.fileids(categories=c):
             doc = NormalizeWords(brown.words(fileids=_id))
             docs_vec.append(doc)
-            docs_cat.append((doc, c))
+            docs_cat_mat.append((doc, c))
+
+        words = NormalizeWords(brown.words(categories=c))
+        bow_vec.append(doc)
+        bow_cat_mat.append((doc, c))
 
     vec_size = 100
     i = 100
-    print("Training Word2Vec...")
+    print("Training Document Word2Vec Model...")
 
-    fil = "w2v_s" + str(vec_size) + "_i_" + str(i) + ".model"
+    fil = "doc_w2v_s" + str(vec_size) + "_i_" + str(i) + ".model"
     if os.path.exists(fil):   
-        word2vec = Word2Vec.load(fil)
+        docw2v = Word2Vec.load(fil)
     else:
-        word2vec = Word2Vec(docs_vec, size=vec_size, iter=i, workers=4)
-        word2vec.save(fil)
+        docw2v = Word2Vec(docs_vec, size=vec_size, iter=i, workers=4)
+        docw2v.save(fil)   
+    
+    print("Training BOW Word2Vec Model...")
+    fil = "bow_w2v_s" + str(vec_size) + "_i_" + str(i) + ".model"
+    if os.path.exists(fil):   
+        boww2v = Word2Vec.load(fil)
+    else:
+        boww2v = Word2Vec(bow_vec, size=vec_size, iter=i, workers=4)
+        boww2v.save(fil)
     
     print("Generating w2v input for models...")
     w2v_docs = []
     w2v_cats = []
     # this block checks if the word in a document is in the word2vec model and if it is get the w2v vector for the word
     # otherwise just fill in zeros for that word.
-    for d, c in docs_cat:
+    for d, c in docs_cat_mat:
         mean = []
         for w in d:
-            if w in word2vec.wv.vocab:
-                mean.append(word2vec.wv.get_vector(w))
+            if w in docw2v.wv.vocab:
+                mean.append(docw2v.wv.get_vector(w))
             else:
                 mean.append(np.zeros(100))
         mean = np.array(mean).mean(axis=0)
@@ -71,22 +86,99 @@ def main():
         w2v_cats.append(c)
 
     # split the documents into training data and test data.
-    train_X, test_X, train_y, test_y = train_test_split(w2v_docs, w2v_cats, test_size=.30, random_state=0)
+    train_X, test_X, train_y, test_y = train_test_split(w2v_docs, w2v_cats, test_size=.30)
 
-    print("Training the Logistic Regression Model...")
-    lg = LogisticRegression(max_iter=50000, C=1e5)
+    # BOW method
+    tr_x = []
+    tr_y = []
+    te_x = []
+    te_y = []
+    for d, c in bow_cat_mat:
+        
+        tr_data, te_data = train_test_split(d, test_size=.3)
+        mean = []
+        for w in tr_data:
+            if w in boww2v.wv.vocab:
+                mean.append(boww2v.wv.get_vector(w))
+            else:
+                mean.append(np.zeros(vec_size))
+        tr_x.append(np.array(mean).mean(axis=0))
+        tr_y.append(c)
+
+        mean = []
+        for w in te_data:
+            if w in boww2v.wv.vocab:
+                mean.append(boww2v.wv.get_vector(w))
+            else:
+                mean.append(np.zeros(vec_size))
+        te_x.append(np.array(mean).mean(axis=0))
+        te_y.append(c)
+
+    print("")
+    print("Training the Logistic Regression Model on Documents...")
+    lg = LogisticRegression(max_iter=5000, C=1e5)
     lg.fit(train_X, train_y)
-    print("Finished training...")
     score = lg.score(test_X, test_y) * 100
-    print("Accuracy score for Logisitic Regression:", "%.2f" % score, "%")
+    print("Accuracy score for Logisitic Regression on Documents:", "%.2f" % score, "%")
 
-    print("Training the Neural Network Model...")
-    clf = MLPClassifier(hidden_layer_sizes=(3, 3, 3, 3, 3), activation="relu", solver="adam", max_iter=100000, learning_rate_init=0.001, n_iter_no_change=500)
+    # The predictions print quite long, so I just gave a score.
+    # y_pred = lg.predict(test_X)
+    # print("Predictions: " + str(list(y_pred)))
+    # print("Ground Truth: " + str(list(test_y)))
+
+    print("")
+    print("Training the Neural Network Model on Documents...")
+    clf = MLPClassifier(hidden_layer_sizes=(100,), activation="relu", solver="adam", max_iter=100000, learning_rate_init=0.001, warm_start=True)
     clf.fit(train_X, train_y)
-    print("Finished training...")
     score = clf.score(test_X, test_y) * 100
-    print("Accuracy score for the Neural Network:", "%.2f" % score, "%")
+    print("Accuracy score for the Neural Network on Documents:", "%.2f" % score, "%")
+
+    print("")
+    print("Training the Neural Network Model on Documents with small NN...")
+    clf = MLPClassifier(hidden_layer_sizes=(3,3,3,3,3), activation="relu", solver="adam", max_iter=100000, learning_rate_init=0.001, warm_start=True)
+    clf.fit(train_X, train_y)
+    score = clf.score(test_X, test_y) * 100
+    print("Accuracy score for the Neural Network on Documents:", "%.2f" % score, "%")
     
+    # The predictions print quite long, so I just gave a score.
+    # y_pred = clf.predict(test_X)
+    # print("Predictions: " + str(list(y_pred)))
+    # print("Ground Truth: " + str(list(test_y)))
+
+    print("")
+    print("#########################################")
+    print("")
+    print("Training the Logistic Regression Model on BOW...")
+    lg = LogisticRegression(max_iter=5000, C=1e5)
+    lg.fit(tr_x, tr_y)
+    score = lg.score(te_x, te_y) * 100
+    print("Accuracy score for Logisitic Regression on BOW:", "%.2f" % score, "%")
+
+    y_pred = lg.predict(te_x)
+    print("Predictions: " + str(list(y_pred)))
+    print("Ground Truth: " + str(list(te_y)))
+
+    print("")
+    print("Training the Neural Network Model on BOW...")
+    clf = MLPClassifier(hidden_layer_sizes=(100,), activation="relu", solver="adam", max_iter=100000, learning_rate_init=0.001, warm_start=True)
+    clf.fit(tr_x, tr_y)
+    score = clf.score(te_x, te_y) * 100
+    print("Accuracy score for the Neural Network on BOW:", "%.2f" % score, "%")
+    
+    y_pred = clf.predict(te_x)
+    print("Predictions: " + str(list(y_pred)))
+    print("Ground Truth: " + str(list(te_y)))
+    
+    print("")
+    print("Training the Neural Network Model on BOW with small NN...")
+    clf = MLPClassifier(hidden_layer_sizes=(3,3,3,3,3), activation="relu", solver="adam", max_iter=100000, learning_rate_init=0.001, warm_start=True)
+    clf.fit(tr_x, tr_y)
+    score = clf.score(te_x, te_y) * 100
+    print("Accuracy score for the Neural Network on BOW:", "%.2f" % score, "%")
+    
+    y_pred = clf.predict(te_x)
+    print("Predictions: " + str(list(y_pred)))
+    print("Ground Truth: " + str(list(te_y)))
 
 def NormalizeWords(words):
     tokens = []
